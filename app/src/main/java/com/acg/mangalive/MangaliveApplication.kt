8bfdecd731833705d7d1569c47e4b2fd35055b57
danilka108 <danilka108@yandex.ru>
2022-08-15 16:00:08 +0700

    refactor MainActivity

--- app/src/main/java/com/acg/mangalive/MangaliveApplication.kt
+++ app/src/main/java/com/acg/mangalive/MangaliveApplication.kt
@@ -1,7 +1,5 @@
 package com.acg.mangalive
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
 class MangaliveApplication : Application() {}
--- app/src/main/java/com/acg/mangalive/ui/MainActivity.kt
+++ app/src/main/java/com/acg/mangalive/ui/MainActivity.kt
@@ -2,15 +2,12 @@ package com.acg.mangalive.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.acg.mangalive.R
 import com.acg.mangalive.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -23,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = findViewById(R.id.MainActivity_BottomAppBar)
 
-        val navHostFragment = binding.MainActivityNavHostFragment as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.MainActivity_NavHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
 
         navView.setupWithNavController(navController)
